db38757cfdeb3f94c5db263b622c9276309c8f08
Idan Yadgar <idanyadgar@gmail.com>
2017-08-29 00:03:33 +0300

    Builds graph from json

--- js/script.js
+++ js/script.js
@@ -1,42 +1,76 @@
+imgByExt = {
+    "xlsx" : "excel",
+    "xls" : "excel",
+    "docx" : "word",
+    "doc" : "word",
+    "ppt" : "ppt",
+    "pptx" : "ppt",
+    "pdf" : "pdf",
+    "png" : "image",
+    "jpg" : "image",
+    "jpeg" : "image"
+};
+var DIR = "pics/";
+
+function parseGraph(root, nodes, edges) {
+    root.parent = 0;
+
+    var queue = [root];
+
+    var id = 1;
+    while (queue.length > 0) {
+        var v = queue.shift();
+
+        var node = {"id": id, "label": v.name};
+        if (v.type === "file") {
+            var ext = v.name.split('.').pop();
+            Object.assign(node, { "shape": 'image', "image": DIR + imgByExt[ext] + '.png' });
+        }
+
+        if (id === 1)
+            Object.assign(node, {
+                "color" : "green",
+                "font" : {
+                    "color" : "white",
+                    "size" : 36
+                }
+            });
+        nodes.push(node);
+
+        if (v.parent) edges.push({"from": v.parent, "to": id});
+
+        for (var n in v["neighbors"]) {
+            v["neighbors"][n].parent = id;
+
+            queue.unshift(v["neighbors"][n]);
+        }
+
+        ++id;
+    }
+}
+
 (function($) {
     $(function () {
-        var DIR = "pics/";
+        $.get("format/web_format.json").done(function(graph) {
+            var nodes = [];
+            var edges = [];
+            parseGraph(graph, nodes, edges);
 
-        var nodes = new vis.DataSet([
-            {id: 1, label: 'Java', color: "green", font: {
-                color: "#fff",
-                size: 32,
-                face: "Segoe UI"
-            }},
-            {id: 2, label: 'Java\nTutorials'},
-            {id: 3, label: 'Advanced\nJava Tutorials'},
-            {id: 4, label: 'Generics.pdf' ,shape: 'image', image: DIR + 'pdf.png'},
-            {id: 5, label: 'Streams.docx' ,shape: 'image', image: DIR + 'word.png'},
-            {id: 6, label: 'Inheritance.ppt' ,shape: 'image', image: DIR + 'ppt.png'},
-            {id: 7, label: 'Some\nSecret Project'},
-            {id: 8, label: 'Project-Image.png' ,shape: 'image', image: DIR + 'image.png'},
-            {id: 9, label: 'Team1.xlsx' ,shape: 'image', image: DIR + 'excel.png'}
-        ]);
+            var container = document.getElementById('mynetwork');
 
-        var edges = new vis.DataSet([
-            {from: 1, to: 2},
-            {from: 2, to: 3},
-            {from: 3, to: 4},
-            {from: 3, to: 5},
-            {from: 2, to: 6},
-            {from: 1, to: 7},
-            {from: 7, to: 8},
-            {from: 7, to: 9}
-        ]);
+            var data = {
+                nodes: new vis.DataSet(nodes),
+                edges: new vis.DataSet(edges)
+            };
+            var options = {
+                nodes : {
+                    font : {
+                        face : "Segoe UI"
+                    }
+                }
+            };
 
-        var container = document.getElementById('mynetwork');
-
-        var data = {
-            nodes: nodes,
-            edges: edges
-        };
-        var options = {};
-
-        var network = new vis.Network(container, data, options);
+            var network = new vis.Network(container, data, options);
+        });
     });
 })(jQuery);
